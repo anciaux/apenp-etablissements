4a9424dc51ca3405e3dda050fe124994a8a72b49
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2019-04-13 21:04:23 +0200

    working map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { Hero } from '../hero';
 
 
@@ -7,14 +7,14 @@ const iconRetinaUrl = 'assets/marker-icon-2x.png';
 const iconUrl = 'assets/marker-icon.png';
 const shadowUrl = 'assets/marker-shadow.png';
 const iconDefault = icon({
-  iconRetinaUrl,
-  iconUrl,
-  shadowUrl,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
+    iconRetinaUrl,
+    iconUrl,
+    shadowUrl,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
 });
 Marker.prototype.options.icon = iconDefault;
 
@@ -30,29 +30,47 @@ export class MapComponent implements OnInit {
 
     @Input() heroes: Hero[];
     @Input() hero: Hero;
+    @Output() selectHero: EventEmitter<any> = new EventEmitter();
 
     mymap = null;
     school_markers = null;
-    
+    markers_per_school = {};
+
     ngOnChanges(changes: SimpleChanges) {
-	if (!changes.hasOwnProperty('heroes'))
-	    return
 	if (!this.heroes)
 	    return
 
-	console.log(changes)
+	if (changes.hasOwnProperty('heroes')){
 
-	this.school_markers.clearLayers();	
-	for (let h of this.heroes){
-	    L.marker([h.lat, h.lng]).addTo(this.school_markers);
+	    console.log(changes);
+	    this.school_markers.clearLayers();	
+	    for (let h of this.heroes){
+		let m = L.marker([h.lat, h.lng]).addTo(this.school_markers);
+		m.bindPopup(`<b>${h.name}</b>`);
+		m.school = h;
+		console.log(h.name);
+		let obj = this;
+		console.log(m, 'AAAAA');
+		m.on('click', function(e) {
+		    console.log(e.target.school, 'AAAAA');
+		    obj.selectHero.emit(e.target.school);
+		});
+		this.markers_per_school[h.name] = m;
+	    }
+	    this.mymap.fitBounds(this.school_markers.getBounds());
+	}
+	if (changes.hasOwnProperty('hero')){
+	    console.log(changes.hero);
+	    this.markers_per_school[this.hero.name].openPopup();
+	    
 	}
     }
     
     constructor() { }
 
     ngOnInit() {
-	this.mymap = L.map('map').setView([46.3820585, 6.2402791], 14);
-	this.school_markers = L.layerGroup().addTo(this.mymap);
+	this.mymap = L.map('map').setView([16.3820585, 6.2402791], 14);
+	this.school_markers = L.featureGroup().addTo(this.mymap);
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(this.mymap);
