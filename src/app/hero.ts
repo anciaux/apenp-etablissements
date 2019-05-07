9b5150b4f70cbd6edc5b88f70dec38260f15373b
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2019-05-07 14:06:44 +0200

    modify to have uape and menu on map

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -5,4 +5,6 @@ export class Hero {
     name: string;
     tel: string;
     address: string;
+    menu: string;
+    email: string;
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,59 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-    {  lat: 46.38727, lng: 6.236625, url: "https://www.epnyonleman.ch/batiment/commune-5-batiment-1/", name: "Ecole du Rocher", tel: "022 557 55 55", address: "Chemin de Prélaz 5, Nyon"},
-    {  lat: 46.38548000000001, lng: 6.221935199999962, url: "https://www.epnyonleman.ch/batiment/commune-4-batiment-1/", name: "Ecole des Tattes d'Oie" , tel: "022 361 06 79", address: "Rte des Tattes d’Oie 97, Nyon"},
-    {  lat: 46.390480367324685, lng: 6.234064874923661, url: "https://www.epnyonleman.ch/batiment/commune-3-batiment-1/", name: "Ecole de La Paix" , tel: "022 361 47 00", address: "Chemin des Plantaz, Nyon"},
-    {  lat: 46.3868192, lng: 6.227677599999993, url: "https://www.epnyonleman.ch/batiment/commune-2-batiment-1/", name: "Ecole du Couchant", tel: "022 557 57 15", address: "Chemin du Couchant 1, Nyon"},
-    {  lat: 46.3808048, lng: 6.23737959999994, url: "https://www.epnyonleman.ch/batiment/ecole-du-centre-ville/", name: "Ecole du Centre-Ville", tel: "022 361 08 12", address: "Rue du Collège 35, Nyon"},
-    {  lat: 46.389999, lng: 6.226466, url: "https://www.epnyonjuraprangins.ch/batiment/commune-1-cossy/", name: "Ecole du Cossy", tel: "021 338 05 25", address: "Chemin de Précossy 6, Nyon"},
-    {  lat: 46.396674, lng: 6.247895, url: "https://www.epnyonjuraprangins.ch/batiment/prangins-les-morettes/", name: "Ecole enfantine des Morettes", tel: "021 338 05 25", address: "Chemin en Purian 6, Prangins"},
-    {  lat: 46.394501, lng: 6.247246, url: "https://www.epnyonjuraprangins.ch/batiment/prangins-la-combe/", name: "Ecole de la Roche-Combe", tel: "021 338 05 25" ,address: "Rue de la Gare 13, Prangins"},
-    {  lat: 46.392985, lng: 6.233821, url: "https://www.epnyonjuraprangins.ch/batiment/nyon-site-de-gai-logis/", name: "Ecole de Gai-Logis", tel: "021 338 05 25", address: "Chemin des Plantaz 15, Nyon"},
-    {  lat: 46.382107, lng: 6.225961, url: "http://www.nyon-marens.ch/", name: "Etablissement secondaire de Nyon-Marens", tel: "022 557 54 00", address: "Route du Stand 5, Nyon"}
+    {  lat: 46.38727, lng: 6.236625, url: "https://www.epnyonleman.ch/batiment/commune-5-batiment-1/", name: "Ecole du Rocher",
+       tel: "022 557 55 55", address: "Chemin de Prélaz 5, Nyon", menu: "http://college-nyon.eldora.ch/fr/accueil.html", email: null},
+    
+    {  lat: 46.38548000000001, lng: 6.221935199999962, url: "https://www.epnyonleman.ch/batiment/commune-4-batiment-1/", name: "Ecole des Tattes d'Oie" ,
+       tel: "022 361 06 79", address: "Rte des Tattes d’Oie 97, Nyon", menu: null, email: null},
+    
+    {  lat: 46.390480367324685, lng: 6.234064874923661, url: "https://www.epnyonleman.ch/batiment/commune-3-batiment-1/", name: "Ecole de La Paix" ,
+       tel: "022 361 47 00", address: "Chemin des Plantaz, Nyon", menu: null, email: null},
+    
+    {  lat: 46.3868192, lng: 6.227677599999993, url: "https://www.epnyonleman.ch/batiment/commune-2-batiment-1/", name: "Ecole du Couchant",
+       tel: "022 557 57 15", address: "Chemin du Couchant 1, Nyon", menu: null, email: null},
+    
+    {  lat: 46.3808048, lng: 6.23737959999994, url: "https://www.epnyonleman.ch/batiment/ecole-du-centre-ville/", name: "Ecole du Centre-Ville",
+       tel: "022 361 08 12", address: "Rue du Collège 35, Nyon", menu: null, email: null},
+    
+    {  lat: 46.389999, lng: 6.226466, url: "https://www.epnyonjuraprangins.ch/batiment/commune-1-cossy/", name: "Ecole du Cossy",
+       tel: "021 338 05 25", address: "Chemin de Précossy 6, Nyon", menu: "http://college-nyon.eldora.ch/fr/accueil.html", email: null},
+    
+    {  lat: 46.396674, lng: 6.247895, url: "https://www.epnyonjuraprangins.ch/batiment/prangins-les-morettes/", name: "Ecole enfantine des Morettes",
+       tel: "021 338 05 25", address: "Chemin en Purian 6, Prangins", menu: null, email: null},
+    
+    {  lat: 46.394501, lng: 6.247246, url: "https://www.epnyonjuraprangins.ch/batiment/prangins-la-combe/", name: "Ecole de la Roche-Combe",
+       tel: "021 338 05 25" ,address: "Rue de la Gare 13, Prangins", menu: "https://prangins.ch/uploads/menu%20école%2029.pdf", email: null},
+    
+    {  lat: 46.392985, lng: 6.233821, url: "https://www.epnyonjuraprangins.ch/batiment/nyon-site-de-gai-logis/", name: "Ecole de Gai-Logis",
+       tel: "021 338 05 25", address: "Chemin des Plantaz 15, Nyon", menu: null, email: null},
+    
+    {  lat: 46.382107, lng: 6.225961, url: "http://www.nyon-marens.ch/", name: "Etablissement secondaire de Nyon-Marens",
+       tel: "022 557 54 00", address: "Route du Stand 5, Nyon", menu: "http://college-nyon.eldora.ch/fr/accueil.html", email: null},
+
+    // UAPE
+    {  lat: 46.394157, lng: 6.249278, url: "http://www.la-fourmiliere-prangins.ch", name: "UAPE La Fourmilière",
+       tel: "022 362 21 14", address: "Chemin en Purian 7, Prangins", menu: "http://www.la-fourmiliere-prangins.ch/formulaires.php", email: "admin@lafourmiliere.ch"},
+    
+    {  lat: 46.386376, lng: 6.227101, url: "https://www.nyon.ch/fr/vivre/enfance-jeunesse/reseau-nyonnais-daccueil-de-jour-0-a-12-ans-939-5419", name: "UAPE du Cèdre Magique",
+       address: "Chemin du Couchant 3, Nyon", tel: "022 316 41 15", email: "cedre.magique@nyon.ch", menu: "http://college-nyon.eldora.ch/fr/accueil.html"},
+    
+    {  lat: 46.384976, lng: 6.240376, url: "https://www.nyon.ch/fr/vivre/enfance-jeunesse/reseau-nyonnais-daccueil-de-jour-0-a-12-ans-939-5419", name: "UAPE des Classes Tartines",
+       address: "Rue des Marchandises 3, Nyon", tel: "022 316 41 20", email: "classes.tartines@nyon.ch ", menu: null},
+    
+    {  lat: 46.391210, lng: 6.237074, url: "https://www.nyon.ch/fr/vivre/enfance-jeunesse/reseau-nyonnais-daccueil-de-jour-0-a-12-ans-939-5419", name: "UAPE du Grand T'Etraz",
+       address: "Route de l'Etraz 50, Nyon", tel: "022 316 41 25", email: "grand.tetraz@nyon.ch", menu: "http://college-nyon.eldora.ch/fr/accueil.html"},
+    
+    {  lat: 46.387126, lng: 6.236495, url: "https://www.nyon.ch/fr/vivre/enfance-jeunesse/reseau-nyonnais-daccueil-de-jour-0-a-12-ans-939-5419", name: "UAPE du Ricochet",
+       address: "Chemin de Prélaz 5, Nyon", tel: "022 316 41 30", email: "ricochet@nyon.ch", menu: "http://college-nyon.eldora.ch/fr/accueil.html"},
+    
+    {  lat: 46.386913, lng: 6.227427, url: "https://www.nyon.ch/fr/vivre/enfance-jeunesse/reseau-nyonnais-daccueil-de-jour-0-a-12-ans-939-5419", name: "UAPE du Justakôté",
+       address: "(Dans l'établissement scolaire du Couchant) Chemin du Couchant 1, Nyon", tel: "(0)22 316 41 35", email: "justakote@nyon.ch", menu: "http://college-nyon.eldora.ch/fr/accueil.html"},
+    
+    {  lat: 46.390109, lng: 6.229536, url: "https://www.nyon.ch/fr/vivre/enfance-jeunesse/reseau-nyonnais-daccueil-de-jour-0-a-12-ans-939-5419", name: "UAPE Les Mangettes",
+       address: "Chemin des Mangettes 4 et Avenue des Eules 3, Nyon", tel: "022 316 41 16", email: "mangettes@nyon.ch", menu: null},
+    
+    {  lat: 46.389778, lng: 6.226419, url: "https://www.nyon.ch/fr/vivre/enfance-jeunesse/reseau-nyonnais-daccueil-de-jour-0-a-12-ans-939-5419", name: "UAPE La Cossynelle",
+       address: "Chemin du Précossy 2, Nyon", tel: "022 316 41 40", email: "cossynelle@nyon.ch", menu: null},
+    
 ];
